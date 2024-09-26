64e4ef0648b3f941963152e621db799415be12d0
Vincelagrimas13 <13vincelagrimas900@gmail.com>
2024-09-26 10:46:45 +0800

    added routing

---
+++ src/components/ContactUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContactUs = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>You can contact us at 13vincelagrimas900@gmail.com or through our contact form.</p>
+    </div>
+  );
+};
+
+export default ContactUs;
